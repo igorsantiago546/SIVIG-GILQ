866c890dc36b8f5626841a322020bce5c3c78d67
Igor Santiago <igor.santiago546@outlook.com>
2026-03-02 20:12:20 -0300

    Inclusão do App.py

---
+++ Script/App.py
@@ -0,0 +1,9 @@
+st.write('**SIVIG-GILQ**')
+st.sidebar.hearder('Título do Cabeçalho')
+
+# Carregamento dos dados direto do Banco (Incluir dentro do GITHUB SQLLITE)
+
+# Seleção de Doenças possíveis
+doença = # base de dados para coluna de Doença
+doença_escolhida = st.sidebar.selectbox('Selecionar Doença', doença)
+
