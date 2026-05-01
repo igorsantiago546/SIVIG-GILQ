b4d945d8f5476b483c90a945a2ea4ae526def2c2
Igor Santiago <igor.santiago546@outlook.com>
2026-04-30 21:09:48 -0300

    Lab_SIVIG_and_Documentation

--- Script/App.py
+++ Script/App.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 
 
-st.write('**SIVIG-GILQ**')
-st.sidebar.header('Título do Cabeçalho')
+#st.write('**SIVIG-GILQ**')
+#st.sidebar.header('Título do Cabeçalho')
 
-df = pd.read_csv('../BaseOriginais/SIVIG_GILQ_UTF8.csv', sep=';', encoding='utf-8')
+#df = pd.read_csv('../BaseOriginais/SIVIG_GILQ_UTF8.csv', sep=';', encoding='utf-8')
 
-doença = df['CID Definitivo (codigo)'].drop_duplicates()
+#doença = df['CID Definitivo (codigo)'].drop_duplicates()
 
-escolha_doença = st.sidebar.selectbox('Escolha a Doença', doença)
+#escolha_doença = st.sidebar.selectbox('Escolha a Doença', doença)
 
 # Seleção de Doenças possíveis
 # doença_escolhida = st.sidebar.selectbox('Selecionar Doença', doença)
